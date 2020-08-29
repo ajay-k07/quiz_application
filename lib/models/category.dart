e21b75f569dc26dbe2919c3dfe1293b7c7bac699
ajay <ajaidonglee@gmail.com>
2020-08-29 15:35:27 +0530

    completed

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -9,28 +9,38 @@ class Category{
 }
 
 final List<Category> categories = [
+
+  Category(17,"Science & Nature", icon: FontAwesomeIcons.microscope),
+  Category(18,"Computer", icon: FontAwesomeIcons.laptopCode),
+  Category(19,"Maths", icon: FontAwesomeIcons.sortNumericDown),
   Category(9,"General Knowledge", icon: FontAwesomeIcons.globeAsia),
+  Category(22,"Geography", icon: FontAwesomeIcons.mountain),
+  Category(23,"History", icon: FontAwesomeIcons.monument),
+  Category(24,"Politics", icon:FontAwesomeIcons.flagUsa),
+
+  Category(25,"Art", icon: FontAwesomeIcons.paintBrush),
   Category(10,"Books", icon: FontAwesomeIcons.bookOpen),
+  Category(21,"Sports", icon: FontAwesomeIcons.footballBall),
+
+
   Category(11,"Film", icon: FontAwesomeIcons.video),
   Category(12,"Music", icon: FontAwesomeIcons.music),
   Category(13,"Musicals & Theatres", icon: FontAwesomeIcons.theaterMasks),
   Category(14,"Television", icon: FontAwesomeIcons.tv),
   Category(15,"Video Games", icon: FontAwesomeIcons.gamepad),
   Category(16,"Board Games", icon: FontAwesomeIcons.chessBoard),
-  Category(17,"Science & Nature", icon: FontAwesomeIcons.microscope),
-  Category(18,"Computer", icon: FontAwesomeIcons.laptopCode),
-  Category(19,"Maths", icon: FontAwesomeIcons.sortNumericDown),
-  Category(20,"Mythology"),
-  Category(21,"Sports", icon: FontAwesomeIcons.footballBall),
-  Category(22,"Geography", icon: FontAwesomeIcons.mountain),
-  Category(23,"History", icon: FontAwesomeIcons.monument),
-  Category(24,"Politics"),
-  Category(25,"Art", icon: FontAwesomeIcons.paintBrush),
-  Category(26,"Celebrities"),
+
+
+
+
   Category(27,"Animals", icon: FontAwesomeIcons.dog),
   Category(28,"Vehicles", icon: FontAwesomeIcons.carAlt),
-  Category(29,"Comics"),
+  Category(29,"Comics",icon: FontAwesomeIcons.bookReader),
   Category(30,"Gadgets", icon: FontAwesomeIcons.mobileAlt),
+
+  // add icon
   Category(31,"Japanese Anime & Manga"),
   Category(32,"Cartoon & Animation"),
+  Category(20,"Mythology"),
+  Category(26,"Celebrities"),
 ];
